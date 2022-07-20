d782c0cd2bb33bc087ccf01c0a502ad8848d2432
Sudipto Shahin <sudipto.shine@gmail.com>
2022-07-20 21:35:21 +0600

    make model migrations and relations between customer and invoices

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'type',
+        'email',
+        'address',
+        'state',
+        'postal_code'
+    ];
+
+    public function invoices() {
+        return $this->hasMany(Invoice::class);
+    }
+}
---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['I', 'B']);
+        $name = $type == "I" ? $this->faker->name() : $this->faker->company();
+
+        return [
+            //
+        ];
+    }
+}
